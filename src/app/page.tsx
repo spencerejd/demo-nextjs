4f43622c57161a52ac90d0a296f17b0e386b4226
Spencer Duvwiama <ejduv@yahoo.co.uk>
2025-12-25 15:43:40 -0800

    Updated files for chapter 7.2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <main className="flex items-center justify-center min-h-screen bg-gray-50">
       <div className="container mx-auto p-4 relative">
-        <div className="bg-white border-2 border-cyan-400 rounded-lg p-6 grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="bg-white border-2 border-emerald-500 rounded-lg p-6 grid grid-cols-1 md:grid-cols-2 gap-4">
           <div className="flex flex-col justify-center">
-            <h1 className={`text-4xlfont-bold mb-4 ${styles.text_blue}`}>Welcome to the Blog</h1>
+            <h1 className={`text-4xlfont-bold mb-4 ${styles.text_green}`}>Welcome to the Blog</h1>
             <p className="text-lg text-gray-700 mb-4">
               Massa urna magnis dignissim id euismod porttitor vitae etiam viverra nunc at adipiscing sit morbi aliquet mauris porttitor nisi, senectus pharetra, ac porttitor orci.
             </p>
-            <a href="/blog/posts" className={`outline outline-1 outline-offset-2 border-cyan-500 text-cyan-500 hover:text-white py-2 px-4 rounded hover:bg-cyan-500 md:w-auto ${styles.fit_content}`}>
+            <a href="/blog/posts" className={`outline outline-1 outline-offset-2 border-emerald-500 text-emerald-500 hover:text-white py-2 px-4 rounded hover:bg-emerald-500 md:w-auto ${styles.fit_content}`}>
               Read
             </a>
           </div>
@@ -34,7 +34,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className={`hidden md:block absolute top-0 right-0 bottom-0 left-2/3 z-0 ${styles.bg_blue}`}></div>
+      <div className={`hidden md:block absolute top-0 right-0 bottom-0 left-2/3 z-0 ${styles.bg_green}`}></div>
     </main>
   );
 }
